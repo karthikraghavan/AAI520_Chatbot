66b60206ee83af4640d524ce796325ba6c6f2bf3
karthikraghavan <karthik.vishwanath@gmail.com>
2024-10-17 23:36:18 -0700

    Update client.py

--- client.py
+++ client.py
@@ -58,7 +58,7 @@ logging.basicConfig(level=logging.INFO,
 def get_ollama_response(input_text):
     logging.info(f"response: {input_text}")
     response = requests.post(
-        "http://localhost:8000/",
+        "https://karthikraghav-squadchatapi.hf.space/",
         json={'input': {'input': input_text}}
     )
     logging.info("I am here")
